999de53236608dd70c54dfadb4c781cf3c7fa44b
Aaron Parker <aaron@stealthpuppy.com>
2026-02-01 09:29:57 +1100

    Add model-based hostname setting and reorganize oobe.sh
    The script now detects the Mac model and sets the hostname accordingly for Mac mini and MacBook Air. Settings sections are reorganized with comments, and minor ordering changes improve clarity and maintainability.

--- macos/oobe.sh
+++ macos/oobe.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+# Dock settings
 defaults write com.apple.dock "tilesize" -int "46"
 defaults write com.apple.dock "autohide" -bool "true"
 defaults write com.apple.dock "autohide-time-modifier" -float "0.5"
@@ -8,6 +9,7 @@ defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
 defaults write com.apple.dock "expose-group-apps" -bool "true"
 killall Dock
 
+# Finder settings
 defaults write com.apple.finder "ShowPathbar" -bool "true"
 defaults write com.apple.finder "_FXSortFoldersFirst" -bool "true"
 defaults write com.apple.finder "FXDefaultSearchScope" -string "SCcf"
@@ -16,33 +18,53 @@ defaults write NSGlobalDomain "NSTableViewDefaultSizeMode" -int "3"
 defaults write com.apple.finder "_FXSortFoldersFirstOnDesktop" -bool "true"
 killall Finder
 
+# Enable spanning of Spaces across multiple displays
 defaults write com.apple.spaces "spans-displays" -bool "true"
 killall SystemUIServer
 
+# Trackpad and TextEdit settings
 defaults write com.apple.AppleMultitouchTrackpad "DragLock" -bool "true"
 defaults write com.apple.AppleMultitouchTrackpad "TrackpadThreeFingerDrag" -bool "true"
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -int "1"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -int "1"
+defaults write com.apple.WindowManager EnableStandardClickToShowDesktop -int "0"
 defaults write com.apple.TextEdit "SmartQuotes" -bool "false"
 defaults write com.apple.CloudSubscriptionFeatures.optIn "545129924" -bool "false"
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -int "1"
-defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -int "1"
-defaults write com.apple.WindowManager EnableStandardClickToShowDesktop -int "0"
 
-scutil --set ComputerName "Einstein"
-scutil --set LocalHostName "einstein"
+# Get the model name of the local machine
+model_name=$(system_profiler SPHardwareDataType | awk '/Model Name/{print $3,$4,$5}')
 
+# Check the model and set hostname accordingly
+if [[ "$model_name" == *"Mac mini"* ]]; then
+    echo "Running command for Mac mini..."
+    scutil --set ComputerName "Marty"
+    scutil --set LocalHostName "marty"
+elif [[ "$model_name" == *"MacBook Air"* ]]; then
+    echo "Running command for MacBook Air..."
+    scutil --set ComputerName "Einstein"
+    scutil --set LocalHostName "einstein"
+else
+    echo "Unknown model: $model_name"
+fi
+
+# Modify screenshot location
 mkdir ~/Pictures/Screenshots
 defaults write com.apple.screencapture "location" -string "~/Pictures/Screenshots"
 defaults write com.apple.screencapture "show-thumbnail" -bool "false"
 killall SystemUIServer
 
+# Zsh
 touch ~/.zprofile
 echo 'echo "reading ~/.zprofile"' >> ~/.zprofile
 touch ~/.zshrc
 echo 'echo "reading ~/.zshrc"' >> ~/.zshrc
 
+# Homebrew
 xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
+# Terminal
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 defaults import com.apple.Terminal ./TerminalPreferences.plist
